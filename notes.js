b811b9a41e7917201bae688550250295fbefdbe8
unknown <erskine.duenas@gmail.com>
2022-08-18 17:41:20 +0800

    first commit

---
+++ notes.js
@@ -0,0 +1,13 @@
+//****Step by step method of the setup:****
+
+$ mkdir todolist-v1
+
+$ cd todolist-v1
+
+$ touch index.html app.js
+
+$ npm init  //to create package.json
+
+$ npm i express body-parser //install express & body-parser
+
+$ npm i ejs //install ejs for templating
